406758d179ec9c4d4afca54631aa9dfd48316101
Payton Swick <payton@foolord.com>
2024-09-21 22:36:25 -0400

    Make asteroids randomly move

--- scenes/asteroid.gd
+++ scenes/asteroid.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
+var min_x = -10
+var min_y = -10
+var max_x = 10
+var max_y = 10
+
 func _ready() -> void:
 	$AnimatedSprite2D.play()
+	velocity = Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y))
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
