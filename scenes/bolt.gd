7b68f81b148d4fc63e7a60b23a3c07884188d4b5
Payton Swick <payton@foolord.com>
2024-09-28 16:16:02 -0400

    Make bolts explode and stop hitting player directly

--- scenes/bolt.gd
+++ scenes/bolt.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 class_name Bolt
 
-signal hit_something
-
 @export var speed: float = 4.0
 
 func _physics_process(_delta: float) -> void:
 	var collision = move_and_collide(velocity)
 	if collision != null:
-		if (collision.get_collider().name == 'PlayerShip'):
-			collision.get_collider()._on_player_hit()
-		hit_something.emit()
-		queue_free()
+		_explode()
+
+func _explode():
+	$Sprite2D.visible = false
+	velocity = Vector2(0,0)
+	$Explosion.restart()
 
 
 func _on_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_explosion_finished() -> void:
 	queue_free()
