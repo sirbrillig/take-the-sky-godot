8b6d17f6449a1ce23ab87fb8e0844da32be857bd
Payton Swick <payton@foolord.com>
2024-09-27 22:30:38 -0400

    Remove asteroids from level2

--- scenes/level_2.gd
+++ scenes/level_2.gd
@@ -1,15 +1,9 @@
 extends Node2D
 
-@export var asteroid: PackedScene
-
-@onready var spawnArea = $LevelArea/LevelAreaRect.shape.extents
-@onready var origin = $LevelArea/LevelAreaRect.global_position -  spawnArea
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HUD.update_health(Global.player_health)
 	$HUD.update_coins(Global.gold_coins)
-	create_asteroids()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,18 +17,3 @@ func _on_player_ship_player_health_changed() -> void:
 
 func _on_player_ship_player_coins_changed() -> void:
 	$HUD.update_coins(Global.gold_coins)
-
-
-func gen_random_pos() -> Vector2:
-	var x = randf_range(origin.x, spawnArea.x)
-	var y = randf_range(origin.y, spawnArea.y)
-	return Vector2(x, y)
-	
-func create_asteroids() -> void:
-	for n in 6:
-		var rock = asteroid.instantiate() as Asteroid
-		var rock_spawn_location = gen_random_pos()
-		rock.position = rock_spawn_location
-		var direction = randf_range(0, 2 * PI)
-		rock.rotation = direction
-		add_child(rock)
